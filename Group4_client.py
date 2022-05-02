ac7ecb65b8d05566bb3bb9c100da9ede161744fc
jays2000 <84200424+jays2000@users.noreply.github.com>
2022-05-02 12:33:53 -0400

    Add files via upload

--- Group4_client.py
+++ Group4_client.py
@@ -1,11 +1,10 @@
-# Client program
-# Connects to the server at port 7777
-# Sends a message to the server, receives a reply and closes the connection
-# Use Python 3 to run
-
 import socket
-from time import sleep
+
+import Cryptodome
 import rsa
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
 
 # create a socket object
 connectionSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,46 +17,24 @@ port = 7777
 # connect to hostname on the port. Note that (host,port) is a tuple.
 connectionSocket.connect((host, port))
 # Receive  the message from the server (receive no more than 1024 bytes)
-receivedBytes = connectionSocket.recv(1024)
-# Decode the bytes into a string (Do this only for strings, not keys)
-n = bytes.decode(receivedBytes)
+n = connectionSocket.recv(1024)
+n = n.decode()
 n = int(n)
-# Print the message
-print("n value: ", n)
+print("n: ", n)
 
-connectionSocket.send('ok'.encode())
-
-receivedBytes = connectionSocket.recv(1024)
-e = receivedBytes.decode()
+e = connectionSocket.recv(1024)
+e = e.decode()
 e = int(e)
-print("e value:", e)
+print("e: ", e)
 
-server_PublicKey = rsa.PublicKey(n, e)
+public_Key = rsa.PublicKey(n, e)
 
-#get user message
-user_Input = input("Please enter a message: ")
-user_Input = user_Input.encode()
+# generate AES key
+key = get_random_bytes(16)
 
-#get message length and send to server as an encrypted string
-messageLength = str((len(user_Input)*8)).encode()
-messageLength = rsa.encrypt(messageLength, server_PublicKey)
-connectionSocket.send(messageLength)
+encrypted_key = rsa.encrypt(key, public_Key)
+
+connectionSocket.send(encrypted_key)
 
 
-while True:
-    try:
-        to_Send = rsa.encrypt(user_Input, server_PublicKey)
-        connectionSocket.send(to_Send)
-        break
-    except OverflowError:                                           #when a message is too long to be encrypted, an OverflowError is thrown
-        to_Send = rsa.encrypt(user_Input[:52], server_PublicKey)    #encrypts 52 because that is the minimum key size
-        user_Input = user_Input[52:]
-        connectionSocket.send(to_Send)
-    sleep(0.5)                                                      #sleep for 0.5 seconds as to not overload the server
 
-receivedBytes = connectionSocket.recv(1024)
-goodbyeMessage = receivedBytes.decode()
-print(goodbyeMessage)
-
-# Close the connection
-connectionSocket.close()
--- Group4_server.py
+++ Group4_server.py
@@ -1,14 +1,6 @@
-# Server program
-# Listens at port 7777
-# Receives the client's message and replies to it and closes the connection
-# Continues listening
-# Use Python 3 to run
-
 import socket
 import rsa
-
-
-global public_Key, private_Key
+import time
 
 #create rsa public and private keys
 public_Key, private_Key = rsa.newkeys(512)
@@ -27,46 +19,33 @@ serverSocket.bind((host, port))
 # start listening for requests
 serverSocket.listen()
 
-while True:
-    print("Waiting for connection.....")
-    # serverSocket accepts if there is a connection request
-    # (note that serverSocket is listening).
-    # Communictaion will be via the clientSocket
-    clientSocket, addr_port = serverSocket.accept()
+clientSocket, addr_port = serverSocket.accept()
 
-    # Print the address and port of the client
-    # addr_port is a tuple that contains both the address and the port number
-    print("Got a connection from " + str(addr_port))
+# Print the address and port of the client
+# addr_port is a tuple that contains both the address and the port number
+print("Got a connection from " + str(addr_port))
 
+# Generate and send RSA Key pair
+server_pub, server_priv = rsa.newkeys(512)
 
-    #Send public key n
-    print(public_Key)
-    n = public_Key.n
-    print("sending n..")
-    n = str(n)
-    n = n.encode()
-    clientSocket.send(n)
+# Get and send n
+n = server_pub.n
+n = str(n)
+n = n.encode()
+print("sending n ...")
+clientSocket.send(n)
+time.sleep(1)
 
-    ok = clientSocket.recv(1024)
+#Get and send e
+e = server_pub.e
+e = str(e)
+e = e.encode()
+print("sending e ...")
+clientSocket.send(e)
 
-    #send Public key e
-    e = public_Key.e
-    print("sending e..")
-    e = str(e)
-    e = e.encode()
-    clientSocket.send(e)
+#Recieve
+encrypted_AES_Key = clientSocket.recv(1024)
 
-    #get length of message - to handle long messages
-    receivedBytes = clientSocket.recv(1024)
-    messageLength = int(rsa.decrypt(receivedBytes, private_Key).decode())
+decrypted_AES_key = rsa.decrypt(encrypted_AES_Key, server_priv)
 
-    amountReceived = 0
-    print("Decrypted message from client: ")
-    while amountReceived < messageLength:
-        receivedBytes = clientSocket.recv(1024)
-        mes = len(receivedBytes)
-        amountReceived += 52*8
-        print(rsa.decrypt(receivedBytes, private_Key).decode())
-
-    #Final message to client
-    clientSocket.send("MESSAGE RECEIVED. Goodbye".encode())
+print(decrypted_AES_key)
